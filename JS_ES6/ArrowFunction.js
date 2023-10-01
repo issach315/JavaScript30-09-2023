7bbf11cca0493ca80ac8eb9ab9494311b7da9e83
issach315 <issachemani227@gmial.com>
2023-10-01 20:45:29 +0530

    arrow function ES6

---
+++ JS_ES6/ArrowFunction.js
@@ -0,0 +1,33 @@
+/*
+The syntax of the arrow function is:
+
+let myFunction = (arg1, arg2, ...argN) => {
+    statement(s)
+}
+
+*/
+
+
+// we are assing the function to the variable 
+let addition = (number1,number2)=>{
+
+     return `addition of ${number1} and ${number2} is = `+ number1+number2;
+}
+
+console.log(addition(10,10));
+
+
+let multiply =(x,y)=>`multiplecation of ${x} and ${y} is = `+ x*y;
+
+console.log(multiply(12,2));
+
+let age = 5;
+
+
+// Arrow function with Expression 
+
+let welcome = (age < 18) ?() => console.log('child') : () => console.log('Adult');
+
+welcome();
+
+
