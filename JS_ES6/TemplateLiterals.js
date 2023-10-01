e71962485928e3de169e701db764f457bf188d27
issach315 <issachemani227@gmial.com>
2023-10-01 21:52:02 +0530

    template literals

---
+++ JS_ES6/TemplateLiterals.js
@@ -0,0 +1,10 @@
+
+const studentName ="Issach";
+
+console.log(`my name is ${studentName}`);
+
+let greetings = 'hello "issach" how are you'; // this is valid 
+console.log(greetings);
+
+let story ='this is a templets  for coming  in new line\n'+' javascript form ES6 ';
+console.log(story);
