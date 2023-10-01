06efdd9e9385eebf8e419db0286000ee55e19ca3
issach315 <issachemani227@gmial.com>
2023-10-01 21:54:14 +0530

    '#'

--- JS_ES6/TemplateLiterals.js
+++ JS_ES6/TemplateLiterals.js
@@ -6,5 +6,5 @@ console.log(`my name is ${studentName}`);
 let greetings = 'hello "issach" how are you'; // this is valid 
 console.log(greetings);
 
-let story ='this is a templets  for coming  in new line\n'+' javascript form ES6 ';
+let story ='this is a templets  for coming  in new line\n'+' javascript form ES6 '; // for new line \n use at the end of line 
 console.log(story);
