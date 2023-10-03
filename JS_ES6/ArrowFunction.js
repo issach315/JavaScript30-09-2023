2ec902e07947f5d2f4bf8c90cec9c3a7c8b3af02
issach315 <issachemani227@gmial.com>
2023-10-03 13:32:57 +0530

    Arrow function

--- JS_ES6/ArrowFunction.js
+++ JS_ES6/ArrowFunction.js
@@ -9,25 +9,30 @@ let myFunction = (arg1, arg2, ...argN) => {
 
 
 // we are assing the function to the variable 
-let addition = (number1,number2)=>{
+let addition = (number1, number2) => {
 
-     return `addition of ${number1} and ${number2} is = `+ number1+number2;
+    return `addition of ${number1} and ${number2} is = ` + number1 + number2;
 }
 
-console.log(addition(10,10));
+console.log(addition(10, 10));
 
 
-let multiply =(x,y)=>`multiplecation of ${x} and ${y} is = `+ x*y;
+let multiply = (x, y) => `multiplecation of ${x} and ${y} is = ` + x * y;
 
-console.log(multiply(12,2));
+console.log(multiply(12, 2));
 
 let age = 5;
 
 
 // Arrow function with Expression 
 
-let welcome = (age < 18) ?() => console.log('child') : () => console.log('Adult');
+let welcome = (age < 18) ? () => console.log('child') : () => console.log('Adult');
 
 welcome();
 
+
+
+// const sa = () => { console.log("hi"); }
+
+// sa();
 
