e9120f4f580cc614144c1eb2238f10eb4a4a3b35
issach315 <issachemani227@gmial.com>
2023-10-06 12:24:34 +0530

    javascript promises

---
+++ JavaScript_Asynchronous/JavaScprit_Promises.js
@@ -0,0 +1,14 @@
+/*
+By definition, a promise is an object that encapsulates the result of an asynchronous operation.
+
+A promise object has a state that can be one of the following:
+
+Pending
+Fulfilled with a value
+Rejected for a reason
+In the beginning, the state of a promise is pending, indicating that the asynchronous operation is in progress.
+ Depending on the result of the asynchronous operation, the state changes to either fulfilled or rejected.
+
+The fulfilled state indicates that the asynchronous operation was completed successfully:
+*/
+
